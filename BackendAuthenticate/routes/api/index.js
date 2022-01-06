319b8e2eb0814ae985c96b440649bc310de82c0e
aaron <aaronshort19@gmail.com>
2022-01-06 12:20:08 -0600

    fixed the api fetch request

--- BackendAuthenticate/routes/api/index.js
+++ BackendAuthenticate/routes/api/index.js
@@ -5,14 +5,9 @@ router.post('/test', function(req, res) {
   });
 
 
-  fetch('/api/test', {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `XSRF-TOKEN`
-    },
-    body: JSON.stringify({ hello: 'world' })
-  }).then(res => res.json()).then(data => console.log(data));
-
-
 module.exports = router;
+
+
+
+
+
