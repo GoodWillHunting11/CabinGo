449af624fc55a657bcecd6dd7a82c4d5da1b23c5
aaron <aaronshort19@gmail.com>
2022-01-06 15:50:46 -0600

    finished phase 3 and tested User Auth, JWT, Middlewares

---
+++ BackendAuthenticate/db/seeders/20220106185741-demo-user.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'andres@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: 'ken@user.io',
+        username: 'Kenneth_Dodson-Knapp',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: 'eric@user.io',
+        username: 'Eric_Cortez',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
