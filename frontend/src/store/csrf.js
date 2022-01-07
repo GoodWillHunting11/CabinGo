647525b0d7ba9a1d6620d82cc54924039f38f577
aaron <aaronshort19@gmail.com>
2022-01-06 19:15:40 -0600

    tested custom csrfFetch with CSRG

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -1,5 +1,9 @@
 import Cookies from 'js-cookie';
 
+export function restoreCSRF() {
+    return csrfFetch('/api/csrf/restore');
+  }
+
 export async function csrfFetch(url, options = {}) {
   // set options.method to 'GET' if there is no method
   options.method = options.method || 'GET';
