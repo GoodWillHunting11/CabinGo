bc3165a63c268181b382a2df58e0dc0af3fce9f5
aaron <aaronshort19@gmail.com>
2022-01-06 21:00:48 -0600

    finished phase 3 and added logout feature and tested. Also added navigation and tested

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,14 @@ const removeUser = () => {
   };
 };
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
