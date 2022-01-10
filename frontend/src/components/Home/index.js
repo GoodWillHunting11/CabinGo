2dc2f3c2255036c7415ef06402a0b357033e7658
aaron <aaronshort19@gmail.com>
2022-01-09 23:38:42 -0600

    fixed styling in spots component, styled the navbar more

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -5,9 +5,9 @@ import './home.css'
 
 function Home() {
     return (
-        <div className="main-container">
+        <div className="home-container">
             <div className="explore-spots">
-                <img id='spots-image' src='https://static.wixstatic.com/media/73107d_e47fade9bd9c42068e06c481d3b318d1~mv2.jpg/v1/fit/w_1000%2Ch_667%2Cal_c%2Cq_80/file.jpg' />
+                <img id='spots-imagee' src='https://static.wixstatic.com/media/73107d_e47fade9bd9c42068e06c481d3b318d1~mv2.jpg/v1/fit/w_1000%2Ch_667%2Cal_c%2Cq_80/file.jpg' />
             </div>
             <div className="explore">
                 <a href='/spots'>
