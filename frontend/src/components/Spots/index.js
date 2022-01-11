b2ba1f4b565869b4f5ffeee8d04cd52be738156c
aaron <aaronshort19@gmail.com>
2022-01-11 12:53:25 -0600

    fixing images not rendering on heroku

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -25,7 +25,7 @@ console.log('all spots',allSpots)
 
                 <div className="spots-container">
                     <div className="image-container">
-                        <img id='spot-image' key={spot.id} src={spot.Images[0].url} />
+                        <img id='spot-image' key={spot.id} src={spot?.Images[0]?.url} />
                     </div>
                     <div className="info">
                         <Link to={`/spots/${spot.id}`}>
