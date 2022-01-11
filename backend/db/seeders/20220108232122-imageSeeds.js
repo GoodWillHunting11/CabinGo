1ca2fdd79dbacc89921ed53a0417f02f539df3c8
aaron <aaronshort19@gmail.com>
2022-01-11 15:59:02 -0600

    added new images to seeder

--- backend/db/seeders/20220108232122-imageSeeds.js
+++ backend/db/seeders/20220108232122-imageSeeds.js
@@ -5,19 +5,19 @@ module.exports = {
       return queryInterface.bulkInsert('Images', [
         {
           spotId: 1,
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDL6I9H1Ap5leiurWjreSmNPluxqPvp6Atlw&usqp=CAU",
+          url: "https://www.sundowncabinrentals.com/images/old_site/cabins/logans_bluff/59375bce-5ff7-42a5-9264-4afb1aeb3cf3.jpg",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           spotId: 2,
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScBK92RrBBGaaKiywOHF85brKjjcMq8ThdQQ&usqp=CAU",
+          url: "https://cdn.liverez.com/5/13922/1/164422/800/1.jpg?v=1/24/2020%206:16:53%20PM",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           spotId: 3,
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn_oDgA3XdpVGp8siSS4fF6jatkrWcteYfLw&usqp=CAU",
+          url: "https://www.beaversbendlodging.com/wp-content/uploads/2020/10/beavers-bend-cabins-oklahoma.jpg",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
