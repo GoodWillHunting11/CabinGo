39883eb1b15738053a4cf1917222ce538a5433ce
aaron <aaronshort19@gmail.com>
2022-01-18 12:23:52 -0600

    fixed nav bar spacing and remove commnets

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -15,14 +15,9 @@ export async function csrfFetch(url, options = {}) {
       options.headers['Content-Type'] || 'application/json';
     options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
   }
-  // call the default window's fetch with the url and the options passed in
   const res = await window.fetch(url, options);
 
-  // if the response status code is 400 or above, then throw an error with the
-    // error being the response
   if (res.status >= 400) throw res;
 
-  // if the response status code is under 400, then return the response to the
-    // next promise chain
   return res;
 }
