ba373e3b7c28f5d12015a67cc7cda9100eb277a7
aaron <aaronshort19@gmail.com>
2022-01-19 00:52:17 -0600

    fixed css

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -5,9 +5,7 @@ export function restoreCSRF() {
   }
 
 export async function csrfFetch(url, options = {}) {
-  // set options.method to 'GET' if there is no method
   options.method = options.method || 'GET';
-  // set options.headers to an empty object if there is no headers
   options.headers = options.headers || {};
 
   if (options.method.toUpperCase() !== 'GET') {
